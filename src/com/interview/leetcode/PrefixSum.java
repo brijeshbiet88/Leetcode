3b618d53501a17a819cf2f28613302c0ba1c3bbf
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-02 12:04:41 +0530

    Prefix Sum Of Array. Output Updated

--- src/com/interview/leetcode/PrefixSum.java
+++ src/com/interview/leetcode/PrefixSum.java
@@ -3,10 +3,20 @@ package com.interview.leetcode;
 public class PrefixSum {
 
 	public static void main(String[] args) {
+		System.out.println("--------Test Case 1----------");
 		int [] a = {1 , 3 , 0 , -2 , 5};
 		prefixSum(a);
 		System.out.println("Array After Prefix Sum");
 		for(int num : a)
+			System.out.print(num+" ");
+		
+		System.out.println();
+		
+		System.out.println("--------Test Case 2----------");
+		int [] b = {10 , 20 , 30 , 40 , 50};
+		prefixSum(b);
+		System.out.println("Array After Prefix Sum");
+		for(int num : b)
 			System.out.print(num+" ");
 
 	}
@@ -17,3 +27,11 @@ public class PrefixSum {
 		}
 	}
 }
+/*
+--------Test Case 1----------
+Array After Prefix Sum
+1 4 4 2 7 
+--------Test Case 2----------
+Array After Prefix Sum
+10 30 60 100 150 
+ */
