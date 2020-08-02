9e21c9084c4d361bbf3dab56144308ebd80ef40f
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-02 11:45:52 +0530

    Subset Sum For Non Negative Elements Equal to K

---
+++ src/com/interview/leetcode/SubsetSumEqualToK.java
@@ -0,0 +1,70 @@
+package com.interview.leetcode;
+
+public class SubsetSumEqualToK {
+
+	public static void main(String[] args) {
+		System.out.println("------------Test Case 1-----------");
+		int [] a = {4, 2, 5 , 1, 6 , 5 ,7 , 2};
+		int k = 21;
+		boolean hasSubset = ifSubsetSumExists(a , k );
+		System.out.println("Does Subset Sum Equal to Sum "+k+" Exists ? "+hasSubset);
+		System.out.println();
+		
+		System.out.println("------------Test Case 2-----------");
+		int [] b = {4, 2, 5 , 1, 6 , 5 ,7 , 2};
+		k = 32 ;
+		hasSubset = ifSubsetSumExists(b , k );
+		System.out.println("Does Subset Sum Equal to Sum "+k+" Exists ? "+hasSubset);
+		System.out.println();
+		
+		System.out.println("------------Test Case 2-----------");
+		int [] c = {4, 2, 5 , 1, 6 , 5 ,7 , 2};
+		k = 52 ;
+		hasSubset = ifSubsetSumExists(c , k );
+		System.out.println("Does Subset Sum Equal to Sum "+k+" Exists ? "+hasSubset);
+		System.out.println();
+		
+		System.out.println("------------Test Case 2-----------");
+		int [] d = {4, 1, 5 , 2, 9 , 5 ,7 , 2};
+		k = 9 ;
+		hasSubset = ifSubsetSumExists(d , k );
+		System.out.println("Does Subset Sum Equal to Sum "+k+" Exists ? "+hasSubset);
+		System.out.println();
+	}
+
+	private static boolean ifSubsetSumExists(int[] a, int k) {
+		int  sum = 0 , leftIndex = 0 ;
+		for(int i = 0 ; i < a.length ; i++) {
+			sum += a[i];
+			
+				while(sum >= k) {
+					if(sum == k) {
+						System.out.println("Start Index :"+leftIndex+" Right Index : "+i);
+						return true;
+					}
+					sum -= a[leftIndex];
+					leftIndex++;
+				}
+			}		
+		return false;
+	}
+
+}
+/*
+------------Test Case 1-----------
+Start Index :3 Right Index : 7
+Does Subset Sum Equal to Sum 21 Exists ? true
+
+------------Test Case 2-----------
+Start Index :0 Right Index : 7
+Does Subset Sum Equal to Sum 32 Exists ? true
+
+------------Test Case 2-----------
+Does Subset Sum Equal to Sum 52 Exists ? false
+
+------------Test Case 2-----------
+Start Index :4 Right Index : 4
+Does Subset Sum Equal to Sum 9 Exists ? true
+
+
+ */
