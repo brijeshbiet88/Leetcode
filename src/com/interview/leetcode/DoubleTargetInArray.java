2c0c27695a29f1f18986a6df930e21068e83e796
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-04 23:08:31 +0530

    Double Target in an array

---
+++ src/com/interview/leetcode/DoubleTargetInArray.java
@@ -0,0 +1,41 @@
+package com.interview.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class DoubleTargetInArray {
+
+	public static void main(String[] args) {
+		System.out.println("-------Test Case 1 --------------");
+		int [] a = {10 , 2 , 5 , 3};
+		System.out.println("Is there m and n such that m = 2*n : "+isDouble(a));
+		System.out.println();
+		
+		System.out.println("-------Test Case 2 --------------");
+		int [] b = {};
+		System.out.println("Is there m and n such that m = 2*n : "+isDouble(b));
+		System.out.println();
+		
+		System.out.println("-------Test Case 3 --------------");
+		int [] c = {1 , 3};
+		System.out.println("Is there m and n such that m = 2*n : "+isDouble(c));
+		System.out.println();
+
+	}
+
+	private static boolean isDouble(int[] a) {
+		Set<Integer> set = new HashSet<>();
+		for(int i = 0 ; i < a.length ; i++) {
+			if(set.contains(2*a[i])) {
+				return true;
+			}
+			else {
+				set.add(a[i]);
+			}
+		}
+		
+		return false;
+	}
+
+}
