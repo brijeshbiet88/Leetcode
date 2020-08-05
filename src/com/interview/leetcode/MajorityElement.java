7fbf24b925919ee2b2d038c3dc07e5f0e47b19bf
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-05 23:21:33 +0530

    Majority Element Using Moore Voting algorithm. Assuming element is there

--- src/com/interview/leetcode/MajorityElement.java
+++ src/com/interview/leetcode/MajorityElement.java
@@ -14,10 +14,34 @@ public class MajorityElement {
 		int [] b = {3 ,3,1,3,2,2 ,7, 8, 9, 2 , 7, 6,2,2,2};
 		System.out.println("Majority Element : "+ majorityElement(b));
 		
-		System.out.println("---------Test Case 2 ---------");
+		System.out.println("---------Test Case 3 ---------");
 		int [] c = {20};
 		System.out.println("Majority Element : "+ majorityElement(c));
+		
+		System.out.println("---------Test Case 4 ---------");
+		int [] d = {20 , 3 , 20 , 5 , 20 , 4 , 20 , 20 , 20 , 6};
+		System.out.println("Majority Element : "+ majorityElement2(d));
+		
+		System.out.println("---------Test Case 5 ---------");
+		int [] e = {10 , 3 , 10 , 3 , 10};
+		System.out.println("Majority Element : "+ majorityElement2(e));
 
+	}
+
+	// Moore Voting works only when majority element is always available
+	
+	private static int majorityElement2(int[] nums) {
+		int count = 0;
+		int candidate = 0;
+		for(int num : nums) {
+			if(count == 0) {
+				candidate = num;
+			}
+			if(num == candidate) {
+				count++;
+			}else count--;
+		}
+		return candidate;
 	}
 
 	public static int majorityElement(int[] nums) {
