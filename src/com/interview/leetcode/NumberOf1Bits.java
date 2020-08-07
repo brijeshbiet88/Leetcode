d8ff1e4f4d403adb20c52dbcafce27047ff46fb9
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-07 21:20:59 +0530

    No of 1 Bits in a Decimal To Binary Integer

---
+++ src/com/interview/leetcode/NumberOf1Bits.java
@@ -0,0 +1,56 @@
+package com.interview.leetcode;
+
+public class NumberOf1Bits {
+
+	public static void main(String[] args) {
+		System.out.println("----------Test Case 1-----------");
+		int n = 0;
+		System.out.println("Number of 1 Bits in Decimal "+n +" Binary "+Integer.toBinaryString(n) +" : "+hammingWeight(n));
+		System.out.println();
+		
+		System.out.println("----------Test Case 2-----------");
+		n = 1000;
+		System.out.println("Number of 1 Bits in Decimal "+n +" Binary "+Integer.toBinaryString(n) +" : "+hammingWeight(n));
+		System.out.println();
+		
+		System.out.println("----------Test Case 3-----------");
+		n = 63;
+		System.out.println("Number of 1 Bits in Decimal "+n +" Binary "+Integer.toBinaryString(n) +" : "+hammingWeight(n));
+		System.out.println();
+		
+		System.out.println("----------Test Case 4-----------");
+		n = 250;
+		System.out.println("Number of 1 Bits in Decimal "+n +" Binary "+Integer.toBinaryString(n) +" : "+hammingWeight(n));
+		System.out.println();
+
+	}
+	
+	public static int hammingWeight(int n) {
+	    int bits = 0;
+	    int mask = 1;
+	    for (int i = 0; i < 32; i++) {
+	        if ((n & mask) != 0) {
+	            bits++;
+	        }
+	        mask = mask << 1;
+	    }
+	    return bits;
+	}
+
+}
+
+/*
+ * ----------Test Case 1-----------
+Number of 1 Bits in Decimal 0 Binary 0 : 0
+
+----------Test Case 2-----------
+Number of 1 Bits in Decimal 1000 Binary 1111101000 : 6
+
+----------Test Case 3-----------
+Number of 1 Bits in Decimal 63 Binary 111111 : 6
+
+----------Test Case 4-----------
+Number of 1 Bits in Decimal 250 Binary 11111010 : 6
+
+
+ */
