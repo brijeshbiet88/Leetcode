5457739c3b246efa0b9d96e13d30eebffb997e05
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-12 22:13:46 +0530

    Count Primes. Refactoring

--- src/com/interview/leetcode/CountPrimes.java
+++ src/com/interview/leetcode/CountPrimes.java
@@ -3,24 +3,24 @@ package com.interview.leetcode;
 public class CountPrimes {
 
 	public static void main(String[] args) {
-		int n = 50;
+		int n = 75;
 		System.out.println("\nCount Of Prime Numbers: "+countPrimes(n));
 
 	}
 	
 	public static int countPrimes(int n) {
-		boolean[] isPrime = new boolean[n];
+		boolean[] isNotPrime = new boolean[n];
 
 		for (int i = 2; i * i < n; i++) {
-			if (isPrime[i])
+			if (isNotPrime[i])
 				continue;
 			for (int j = i * i; j < n; j += i) {
-				isPrime[j] = true;
+				isNotPrime[j] = true;
 			}
 		}
 		int count = 0;
 		for (int i = 2; i < n; i++) {
-			if (!isPrime[i]) {
+			if (!isNotPrime[i]) {
 				System.out.print(i+" ");
 				count++;
 			}
@@ -30,7 +30,6 @@ public class CountPrimes {
 
 }
 /*
-
-2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
-Count Of Prime Numbers: 15
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 
+Count Of Prime Numbers: 21
 */
