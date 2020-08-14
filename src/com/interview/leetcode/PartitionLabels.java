2e4a5a899595e2d8a759a3e3514ece4270ce522a
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-14 14:06:03 +0530

    Partition Labels

---
+++ src/com/interview/leetcode/PartitionLabels.java
@@ -0,0 +1,85 @@
+package com.interview.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PartitionLabels {
+
+	public static void main(String[] args) {
+		System.out.println("------------------Test Case 1------------------");
+		String S = "ababcbacadefegdehijhklij";
+		List<Integer> list = partitionLabels(S);
+		System.out.println("Partition Labels in String : "+S);
+		list.forEach(s->System.out.print(s+" "));
+		list.clear();
+		System.out.println();
+		
+		System.out.println("------------------Test Case 2------------------");
+		S = "rabcbcaghghilmmlnoppns";
+		list = partitionLabels(S);
+		System.out.println("Partition Labels in String : "+S);
+		list.forEach(s->System.out.print(s+" "));
+		list.clear();
+		System.out.println();
+		
+		System.out.println("------------------Test Case 3------------------");
+		S = "abcdefghijklmnopqrstuvwxyz";
+		list = partitionLabels(S);
+		System.out.println("Partition Labels in String : "+S);
+		list.forEach(s->System.out.print(s+" "));
+		list.clear();
+		System.out.println();
+
+	}
+	
+	public static List<Integer> partitionLabels(String S) {
+		List<Integer> list = new ArrayList<Integer>();
+		Set<Character> charSet = new HashSet<Character>();
+
+		int i = 0;
+		int start = 0;
+		int target = 0;
+		while (i < S.length()) {
+			
+			int startIndex = start;
+			while(start < S.length() && target < S.length()) {
+				char c = S.charAt(start);
+				if(!charSet.contains(c)) {
+					charSet.add(c);
+				}
+				int indexOfChar = S.indexOf(c , start+1);
+				if(indexOfChar != -1 && target < indexOfChar) {
+					target = indexOfChar;
+				}else if(indexOfChar == -1 && target < start) {
+					target = start;
+				}
+				if(start == target) {
+					list.add(target-startIndex+1);
+						start++;
+					charSet.clear();
+					break;
+				}
+		
+			start++;
+		}
+			i = start;
+
+	}
+		return list;
+	}
+}
+
+/*
+------------------Test Case 1------------------
+Partition Labels in String : ababcbacadefegdehijhklij
+9 7 8 
+------------------Test Case 2------------------
+Partition Labels in String : rabcbcaghghilmmlnoppns
+1 6 4 1 4 5 1 
+------------------Test Case 3------------------
+Partition Labels in String : abcdefghijklmnopqrstuvwxyz
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+
+ * */
