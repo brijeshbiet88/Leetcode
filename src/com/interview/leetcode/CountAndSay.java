b360e32f828441a17eb712bd1700238d051da09d
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-26 10:33:00 +0530

    Count and Say. Using DP

--- src/com/interview/leetcode/CountAndSay.java
+++ src/com/interview/leetcode/CountAndSay.java
@@ -3,8 +3,17 @@ package com.interview.leetcode;
 public class CountAndSay {
 
 	public static void main(String[] args) {
+		System.out.println("----------Test Case 1 -----------------");
+		System.out.println("Using Recursion");
 		for(int n = 1; n <= 10 ; n++) {
 			System.out.println(countAndSay(n));
+		}
+		System.out.println();
+		System.out.println("-------------Test Case 2---------------");
+		System.out.println("Using Dynamic Programming");
+		
+		for(int n = 1; n <= 10 ; n++) {
+			System.out.println(countAndSayDp(n));
 		}
 		
 	}
@@ -46,9 +55,49 @@ public class CountAndSay {
 		return sb.toString();
 
 	}
+	
+	public static String countAndSayDp(int n) {
+	String [] dp = new String[n+1];
+	dp[1] = "1";
+	int count = 1;
+	
+	for(int index = 2; index <=n ;index++) {
+		StringBuilder sb = new StringBuilder();
+		int i = 1;
+		String str = dp[index-1];
+		if(str.length() == 1) {
+			sb.append(1);
+			sb.append(str.charAt(0));
+		}
+		while (i < str.length()) {
+			
+			char c = str.charAt(i-1);
+			while (i < str.length() &&  c == str.charAt(i)) {
+				count++;
+				i++;
+			}
+			
+			sb.append(count);
+			sb.append(str.charAt(i - 1));
+			count = 1;
+			i++;
+		}
+		
+		if(str.length() >= 2 && str.charAt(str.length()-2) != str.charAt(str.length()-1)) {
+			sb.append("1");
+			sb.append(str.charAt(str.length()-1));
+		}
+		dp[index] = sb.toString();
+	}
+	
+	return dp[n];
+	}
 
 }
 /*
+
+----------Test Case 1 -----------------
+Using Recursion
 1
 11
 21
@@ -59,5 +108,19 @@ public class CountAndSay {
 1113213211
 31131211131221
 13211311123113112211
+
+-------------Test Case 2---------------
+Using Dynamic Programming
+1
+11
+21
+1211
+111221
+312211
+13112221
+1113213211
+31131211131221
+13211311123113112211
+
 
  * */
